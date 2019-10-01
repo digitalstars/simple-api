44a27b6ac85676a43c4e37f905bab895dd6010f5
digitalstars <46852707+digitalstars@users.noreply.github.com>
2019-10-02 01:53:30 +0300

    Новая версия

--- src/SimpleAPI.php
+++ src/SimpleAPI.php
@@ -1,41 +1,53 @@
 <?php
+
 namespace DigitalStars;
 
 class SimpleAPI {
     private $data = [];
+    private $flag = 0;
     public $answer = [];
+    public $module = '';
 
     public function __construct() {
         $this->data = $_POST + $_GET;
-        if(!isset($this->data['module']))
-            $this->exit(['error' => 'missed params']);
+        if(isset($this->data['json_data']))
+            $this->data = json_decode($this->data['json_data'], true);
+
+        if (!isset($this->data['module'])) {
+            $this->answer['error'] = 'missed module';
+            exit();
+        } else
+            $this->module = $this->data['module'];
     }
 
     public function __destruct() {
-        $this->exit($this->answer);
+        header('Content-Type: application/json');
+        exit(json_encode($this->answer));
     }
 
-    public function module($name, $params, $anon) {
-        if($this->data['module'] == $name) {
-            if ($this->array_keys_exist($params)) {
-                $anon($this->data);
+    public function error($text) {
+        $this->answer['error'] = $text;
+        exit();
+    }
+
+    public function params($params) {
+        if(!$this->flag) {
+            $this->flag = 1;
+            if ($this->array_keys_exist($params))
+                return true;
+            else {
+                $this->answer['error'] = 'missed params';
                 exit();
             }
-            else
-                $this->exit(['error' => 'missed params']);
-        }
+        } else
+            exit();
     }
 
-    private function exit($array) {
-        header('Content-Type: application/json');
-        exit(json_encode($array));
-    }
-
-    private function array_keys_exist($keys){
-        foreach($keys as $key){
-            if($key{0} == '?')
+    private function array_keys_exist($keys) {
+        foreach ($keys as $key) {
+            if ($key{0} == '?')
                 continue;
-            if(!array_key_exists($key, $this->data))
+            if (!array_key_exists($key, $this->data) | !isset($this->data[$key]))
                 return false;
         }
         return true;
